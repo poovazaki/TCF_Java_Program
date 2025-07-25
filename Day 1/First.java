8669809518a895515b8b5d39af1a7a29f77c0225
poovazaki <poovazakia@gmail.com>
2025-07-25 18:19:46 +0530

    Add files via upload

---
+++ Day 1/First.java
@@ -0,0 +1,9 @@
+class First{
+public static void main(String[]args){
+String name="poo";
+int rollno=123;
+System.out.println("my name:"+name);
+System.out.println("roll number:"+rollno);
+}
+}
+
---
+++ Day 1/Second.java
@@ -0,0 +1,7 @@
+public class Second {
+    public static void main(String[] args) {
+        int n=25;
+        System.out.println(n/10);
+        System.out.println(n%10);
+    }
+}
